f8db842f9dd29910085daaa5e7fd31be58a80692
Jhonnyoptimuz <jhonnyoptimuz@gmail.com>
2026-06-24 22:55:39 -0400

    LIMPIEZA TOTAL: Eliminando conflictos de Git en todos los archivos

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
@@ -7,13 +6,3 @@ const config = {
 };
 
 export default config;
-=======
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
->>>>>>> 13226a963e9fc2c986b4ed98662932f6e386517c
